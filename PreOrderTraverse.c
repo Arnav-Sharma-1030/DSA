d90492735a47ce3f17c5bd7fbe607d37fe92faf6
Arnav Sharma <arnavsharma7352@gmail.com>
2023-04-21 22:38:04 +0530

    Add files via upload
     the node struct represents a node in the binary search tree, with three fields: data, left, and right. The data field stores the value of the node, while the left and right fields point to the left and right subtrees, respectively.
    
    The createNode function creates a new node with the specified data value and returns a pointer to the new node. The insertNode function inserts a new node with the specified data value into the tree rooted at root, following the rules of a binary search tree. The preorderTraversal function performs a pre-order traversal of the tree rooted at root, printing out the data values of the nodes in pre-order.
    
    In the main function, we first create an empty binary search tree by setting the root pointer to NULL. We then insert some nodes into the tree using the insertNode function, and finally perform a pre-order traversal of the tree using the preorderTraversal function.

---
+++ PreOrderTraverse.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* createNode(int data) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct node* insertNode(struct node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+
+void preorderTraversal(struct node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct node* root = NULL;
+    root = insertNode(root, 50);
+    insertNode(root, 30);
+    insertNode(root, 20);
+    insertNode(root, 40);
+    insertNode(root, 70);
+    insertNode(root, 60);
+    insertNode(root, 80);
+    printf("Preorder traversal of the binary search tree is: ");
+    preorderTraversal(root);
+    return 0;
+}
